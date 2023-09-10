9d6a1b484f581c2fedea8b26d75baaf806851d47
@AILearner93 <63361144+AILearner93@users.noreply.github.com>
2023-09-10 21:58:22 +0100

    This include changes for data types and tests

--- fw_recursion.py
+++ fw_recursion.py
@@ -1,31 +1,45 @@
 import sys
 
 NO_PATH = sys.maxsize
-graph = [[0, 7, NO_PATH, 8],
-[NO_PATH, 0, 5, NO_PATH],
-[NO_PATH, NO_PATH, 0, 2],
-[NO_PATH, NO_PATH, NO_PATH,1]]
-MAX_LENGTH = len(graph[0])
 
 def FW_recursion(distance,int_Node=0):
     """
     A simple implementation of Floyd's algorithm with a recursive funtion#
-    this takes in the input of the matrix and also the int node if the into node is present.
+    this takes in the input of the graph array and also the int node if the into node is present.
 
     the function will then compute the minimum distance between two of the nodes
+
+    It also carries out check for if the graph is square and if incorrect datatypes are in the graph
     """
-    #get the length of the matrix, must be square for it to work.
     distance_length =len(distance)
 
+    for row in distance:
+        if not all(isinstance(elem, (int,float)) for elem in row) :
+            raise TypeError("The elements in this graph need to be of type int or floats")
+
+
+
+    #get the length of the matrix, must be square for it to work
     if distance_length == int_Node:
+        for i in range(distance_length):
+            for j in range(distance_length):
+                if distance [i][j] < 0:
+                    raise ValueError("Please ensure there are no negative weights in the graph")
         print(distance)
         return distance
     else:
-        for start_node in range(distance_length):
-            for end_node in range(distance_length):
-                distance[start_node][end_node] = min(distance[start_node][end_node],
-            distance[start_node][int_Node] + distance[int_Node][end_node] )
+        for i in range(distance_length):
+            for j in range(distance_length):
+                distance[i][j] = min(distance[i][j],
+            distance[i][int_Node] + distance[int_Node][j] )
         int_Node+=1
-        FW_recursion(distance,int_Node)
+        return FW_recursion(distance,int_Node)
 
-MinDisGraph=FW_recursion(graph)
+initial_graph = [
+            [0, NO_PATH, NO_PATH],
+            [2, 0, 9],
+            [NO_PATH, 1, 0]
+]
+
+FW_recursion(initial_graph)
+
